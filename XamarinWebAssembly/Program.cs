6a5c94fc30ff916501ae506d2dfc7ff91c9143ca
Luis David De la cruz <delacruz_luis@outlook.es>
2018-05-30 23:05:54 -0500

    Web Assembly Files

---
+++ XamarinWebAssembly/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Ooui;
+using Xamarin.Forms;
+
+namespace XamarinWebAssembly
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            // Initialize Xamarin.Forms
+            Forms.Init ();
+            // Create the UI
+            var page = new ContentPage();
+            var stack = new StackLayout();
+            var button = new Xamarin.Forms.Button {
+                Text = "Touch!"
+            };
+            stack.Children.Add(button);
+            page.Content = stack;
+
+            // Add some logic to it
+            var count = 0;
+            button.Clicked += (s, e) => {
+                count++;
+                button.Text = $"Clicked {count} times";
+            };
+
+            // Publish a root element to be displayed
+            UI.Publish("/", page.GetOouiElement());
+        }
+    }
+}
